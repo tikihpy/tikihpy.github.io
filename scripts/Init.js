5ab7235c9fc2d8afb59c94eaf2e92dfc8eeedcdb
kina <tikihpy@163.com>
2019-05-07 16:37:44 +0800

    2019050702

--- scripts/Init.js
+++ scripts/Init.js
@@ -197,15 +197,4 @@ function getPage(){
   )
 }
 
-function addLoadEvent(func){
-  var oldload = window.onload;
-  if(typeof window.onload != 'function'){
-      window.onload = func;
-  }else{
-      window.onload = function(){
-          oldload();
-          func();
-      }
-  }
-}
 
